457ce3d8e747234171420b69b42b21420c34532d
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-02-09 19:39:43 +0300

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import path from 'path';
+import vercel from '@sveltejs/adapter-vercel';
 
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    adapter: vercel(),
     vite: {
       resolve: {
         alias: {
